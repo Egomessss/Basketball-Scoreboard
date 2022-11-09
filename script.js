c656157fae272afa053a4d8161b8a67af2a7408f
Egomessss <ed.gomes97@gmail.com>
2022-11-09 13:07:30 +0000

    removed comments

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-// criar variaveis para os dois CSSCounterStyleRule(home e guest) e para a contagem
-
 let homeEl = document.getElementById("home-el");
 let guestEl = document.getElementById("guest-el");
 let newGameEl = document.getElementById("newgame-el");
 
 let count = 0;
-
-// criar funcao para mudar o score de home
-
-//  3 funcões tem que fazer o incremento e alterar o valor do score de home
 
 function addhOne() {
   count += 1;
@@ -24,9 +18,7 @@ function addhThree() {
   count += 3;
   homeEl.textContent = count;
 }
-// criar funcao para mudar o score de  guest
 
-// 3 funcões  tem que fazer o incremento e alterar o valor do score de guest
 function addgOne() {
   count += 1;
   guestEl.textContent = count;
@@ -41,8 +33,6 @@ function addgThree() {
   count += 3;
   guestEl.textContent = count;
 }
-
-// adicionar botão que tem a funcionalidade dar reset no jogo para iniciar nova pontuação
 
 function newGame() {
   homeEl.textContent = 0;
